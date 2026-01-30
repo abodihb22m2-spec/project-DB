9af15f419fdf1d5880e970c8b99771c213bf0f4c
abodihb22m2-spec <AbodiHB22m2@gmail.com>
2026-01-30 16:33:57 +0300

    fix: remove deprecated mongoose options and fix connection

--- routes/allRoutes.js
+++ routes/allRoutes.js
@@ -5,12 +5,7 @@ const moment = require('moment');
 const mongoose = require("mongoose");
 
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(() => console.log("DB connected"))
-.catch(err => console.log("DB connection error:", err));
+mongoose.connect(process.env.MONGODB_URI);
 
 router.get("/", async (req, res) => {
     try {
